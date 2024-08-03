17d1fd567353784befc10a0a97500e1b455cfb60
JamesPotenciano <146494940+JamesPotenciano@users.noreply.github.com>
2024-08-03 17:40:57 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="logo.png">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>MYKOPIKO</title>
+</head>
+<body>
+<header class="headermain">
+    <nav>
+        <div class="brand-container">
+            <img src="mainlogo.png" class="logo">
+            <h1 class="brand-title">MYKO</h1>
+            <h1 class="brand-title2">PIKO</h1>
+        </div>
+        <div class="links-container">
+            <a class="link" href="Main_Page.html">HOME</a>
+            <a class="link" href="#">ABOUT US</a>
+            <a class="link" href="#">CONTACT</a>
+        </div>
+    </nav>
+</header>
---
+++ second_page.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="shortcut icon" type="image/x-icon" href="logo.png">
+
+    <title>MYKOPIKO</title>
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <h1>MENU</h1>
+    <a href=""class="checkout">Checkout</a>
+    <section class="container">
+        <div class="card">
+            <div class="card-image coffee-1">
+                <div class="card-text">
+                    <h2>Caramel Machiatto</h2>
+                    <h5>250PHP</h5>
+                </div>
+            </div>
+            <p>
+            A latte-style beverage served hot or iced made with milk, vanilla syrup, and topped with espresso and caramel sauce
+            </p>
+            <a href=""class ="button1">Add To Cart</a>
+        </div>
+
+        <div class="card">
+            <div class="card-image coffee-2">
+                <div class="card-text">
+                    <h2>Vanilla Latte</h2>
+                    <h5>220PHP</h5>
+                </div>
+            </div>
+            <p>
+            The Vanilla Latte is a classic latte It is made with full-bodied espresso, creamy steamed milk and classic vanilla syrup
+            </p>
+            <a href=""class ="button1">Add To Cart</a>
+        </div>
+
+        <div class="card">
+            <div class="card-image coffee-3">
+                <div class="card-text">
+                    <h2>Frappe</h2>
+                    <h5>200PHP</h5>
+                </div>
+            </div>
+            <p>
+            This delicious drink is generally made of water, espresso, sugar, milk, ice and is shaken, blended or beaten to combine the ingredients.
+            </p>
+            <a href="" class ="button2">Add To Cart</a>
+        </div>
+    </section>
+
+
+    <section class="container1">
+        <div class="card1">
+            <div class="card-image1 coffee-4">
+                <div class="card-text1">
+                    <h2>Mocha Coffee</h2>
+                    <h5>230PHP</h5>
+                </div>
+            </div>
+            <p>
+            A latte-style beverage served hot or iced made with milk, vanilla syrup, and topped with espresso and caramel sauce
+            </p>
+            <a href=""class ="button2">Add To Cart</a>
+        </div>
+
+        <div class="card1">
+            <div class="card-image1 coffee-5">
+                <div class="card-text1">
+                    <h2>Capuccino</h2>
+                    <h5>225PHP</h5>
+                </div>
+            </div>
+            <p>
+            The Vanilla Latte is a classic latte It is made with full-bodied espresso, creamy steamed milk and classic vanilla syrup
+            </p>
+            <a href=""class ="button2">Add To Cart</a>
+        </div>
+
+        <div class="card1">
+            <div class="card-image1 coffee-6">
+                <div class="card-text1">
+                    <h2>Cookies And Cream Frappe</h2>
+                    <h5>210PHP</h5>
+                </div>
+            </div>
+            <p>
+            This delicious drink is generally made of water, espresso, sugar, milk, ice and is shaken, blended or beaten to combine the ingredients.
+            </p>
+            <a href="" class ="button2">Add To Cart</a>
+        </div>
+    </section>
+</body>
+</html>
